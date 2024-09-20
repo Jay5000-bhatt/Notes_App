9cc039683b846ed12830ea1fe0a5ae5bf31398cf
Jay5000-bhatt <bhattjay114@gmail.com>
2024-09-21 01:45:12 +0530

    upgraded secure site

--- Backend/utils/JWT.Token.js
+++ Backend/utils/JWT.Token.js
@@ -10,7 +10,7 @@ export const generateTokenAndSetCookie = (res, userId) => {
 
   res.cookie("token", token, {
     httpOnly: true, // Prevent client-side JavaScript from accessing the cookie
-    secure: process.env.NODE_ENV === "production", // Secure flag only for HTTPS in production
+    secure: true, // Secure flag only for HTTPS in production
     sameSite: "None", // In development, you can use "Lax"
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
   });
